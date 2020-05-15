0347cc1bc968d9213173a926006b06779a735e1e
Máximo Sánchez <id569723@outlook.com>
2020-05-15 01:00:47 -0500

    Primer Commit - v1.0

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace MVC___Github.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
